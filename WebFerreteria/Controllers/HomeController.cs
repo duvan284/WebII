a4cf01151ccc4ef3874fc6a2e20d2e05fff0d4df
duvan <duvan.3801821097@ucaldas.edu.co>
2025-04-30 20:12:55 -0500

    Cambio controlador home

--- WebFerreteria/Controllers/HomeController.cs
+++ WebFerreteria/Controllers/HomeController.cs
@@ -13,7 +13,7 @@ namespace WebFerreteria.Controllers
             _logger = logger;
         }
 
-        public IActionResult Index()
+        public IActionResult Index1()
         {
             return View();
         }
